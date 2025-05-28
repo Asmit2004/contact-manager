b23379cb54bb3f487f803eedf583913ddd075f0f
Asmit Jana <asmitjana16@gmail.com>
2025-05-29 03:51:59 +0530

    Update delete.php

--- delete.php
+++ delete.php
@@ -7,13 +7,11 @@ if (!isset($_GET['id']) || empty($_GET['id'])) {
 
 $id = intval($_GET['id']);
 
-// Prepare and execute delete
 $stmt = $conn->prepare("DELETE FROM contacts WHERE id = ?");
 $stmt->bind_param("i", $id);
 
 if ($stmt->execute()) {
     $stmt->close();
-    // Redirect back to index.php after deletion
     header("Location: index.php");
     exit;
 } else {
